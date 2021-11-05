5fc32717a266c7789d882bc25e615606f7cc0ffc
Steve Geinitz <geinitz@msudenver.edu>
2021-11-05 17:42:48 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## Assign 5
+
+### Coding directions
+
+In this fifth assignment we will be implementing a Genetic Algorithm to solve
+the Traveling Salesperson problem for a complete, weighted, and directed graph. 
+
+### Required: Function to be implemented
+
+Note that only one function is required for this assignemnt. This is: 
+
+
+* __TSPwGenAlgo(W, population_size=50, mutation_rate=0.01, explore_rate=0.5)__
+  * input: an adjacency weight matrix, population size to use, mutation rate,
+    and exploration rate (lower exploration rate means a smaller group of 'fit'
+    individuals should be used for reproduction, a larger exploration rate means
+    a larger group of individuals should be used - you can implement this
+    however you, but it should have some affect on how reproduction is done)
+  * output/return: a dictionary containing the solution path and distance, and
+    a list with the shortest distance found in each generation (see assign5.py) 
+
+### Optional: Each of these functions impemented is +10 pts
+
+* __TSPwDynProg(W)__ 
+  * input: adjacency weight matrix, W such that W[i][j] is the weight from vertex i to vertex j
+  * output/return: a dictionary containing the solution path and distance (see assign5.py) 
+
+* __TSPwBandB(W)__ carry out Dijkstra's algorithm on a graph represented by its weight matrix
+  * input: adjacency weight matrix, W such that W[i][j] is the weight from vertex i to vertex j
+  * output/return: a dictionary containing the solution path and distance (see assign5.py) 
+
+
+Since your program will not be run through unit test on GitHub, there will also
+not be any linting checks. However, be sure to document your code, including any
+auxiliary functions that you use. You should replace/change the existing
+comments that are there to help you get started. 
+
---
+++ assign5.py
@@ -0,0 +1,127 @@
+"""
+For this assignment there is no automated testing. You will instead submit
+your *.py file in Canvas. I will download and test your program from Canvas.
+"""
+
+import time
+
+
+def adjMatFromFile(filename):
+    """ Create an adj/weight matrix from a file with verts, neighbors, and weights. """
+    f = open(filename, "r")
+    n_verts = int(f.readline())
+    print(f" n_verts = {n_verts}")
+    adjmat = [[None] * n_verts for i in range(n_verts)]
+    for i in range(n_verts):
+        adjmat[i][i] = 0
+    for line in f:
+        int_list = [int(i) for i in line.split()]
+        vert = int_list.pop(0)
+        assert len(int_list) % 2 == 0
+        n_neighbors = len(int_list) // 2
+        neighbors = [int_list[n] for n in range(0, len(int_list), 2)]
+        distances = [int_list[d] for d in range(1, len(int_list), 2)]
+        for i in range(n_neighbors):
+            adjmat[vert][neighbors[i]] = distances[i]
+    f.close()
+    return adjmat
+
+
+def TSPwGenAlgo(g, population_size=50, mutation_rate=0.01, explore_rate=0.5):
+    """ A genetic algorithm to attempt to find an optimal solution to TSP  """
+
+    solution_cycle_distance = None # the distance of the final solution cycle/path
+    solution_cycle_path = [] # the sequence of vertices representing final sol path to be returned
+    shortest_path_each_generation = [] # store shortest path found in each generation
+
+    # create individual members of the population
+
+    # initialize individuals to an initial 'solution'
+
+    # loop for x number of generations (with possibly other early-stopping criteria)
+
+        # calculate fitness of each individual in the population
+        # (and append distance of the 'fittest' to shortest_path_each_generation)
+
+        # select the individuals to be used to spawn the generation, then create
+        # individuals of the new generation (using some form of crossover)
+
+        # allow for mutations (this should not happen too often)
+
+        # ...
+
+    # calculate and verify final solution, and update solution_cycle_distance,
+    # solution_path, etc.
+
+    # ...
+
+    return {
+            'solution': solution_cycle_path,
+            'solution_distance': solution_cycle_distance,
+            'evolution': shortest_path_each_generation
+           }
+
+
+def TSPwDynProg(g):
+    """ (10pts extra credit) A dynamic programming approach to solve TSP """
+    solution_cycle_distance = None # the distance of the final solution cycle/path
+    solution_cycle_path = [] # the sequence of vertices representing final sol path to be returned
+
+    #...
+
+    return {
+            'solution': solution_cycle_path,
+            'solution_distance': solution_cycle_distance,
+           }
+
+
+def TSPwBandB(g):
+    """ (10pts extra credit) A branch and bound approach to solve TSP """
+    solution_cycle_distance = None # the distance of the final solution cycle/path
+    solution_cycle_path = [] # the sequence of vertices representing final sol path to be returned
+
+    #...
+
+    return {
+            'solution': solution_cycle_path,
+            'solution_distance': solution_cycle_distance,
+           }
+
+
+def assign05_main():
+    """ Load the graph (change the filename when you're ready to test larger ones) """
+    g = adjMatFromFile("complete_graph_n08.txt")
+
+    # Run genetic algorithm to find best solution possible
+    start_time = time.time()
+    res_ga = TSPwGenAlgo(g)
+    elapsed_time_ga = time.time() - start_time
+    print(f"GenAlgo runtime: {elapsed_time_ga:.2f}")
+    print(f"  sol dist: {res_ga['solution_distance']}")
+    print(f"  sol path: {res_ga['solution']}")
+
+    # (Try to) run Dynamic Programming algorithm only when n_verts <= 10
+    if len(g) <= 10:
+        start_time = time.time()
+        res_dyn_prog = TSPwDynProg(g)
+        elapsed_time = time.time() - start_time
+        if len(res_dyn_prog['solution']) == len(g) + 1:
+            print(f"Dyn Prog runtime: {elapsed_time:.2f}")
+            print(f"  sol dist: {res_dyn_prog['solution_distance']}")
+            print(f"  sol path: {res_dyn_prog['solution']}")
+
+    # (Try to) run Branch and Bound only when n_verts <= 10
+    if len(g) <= 10:
+        start_time = time.time()
+        res_bnb = TSPwBandB(g)
+        elapsed_time = time.time() - start_time
+        if len(res_bnb['solution']) == len(g) + 1:
+            print(f"Branch & Bound runtime: {elapsed_time:.2f}")
+            print(f"  sol dist: {res_bnb['solution_distance']}")
+            print(f"  sol path: {res_bnb['solution']}")
+
+
+# Check if the program is being run directly (i.e. not being imported)
+if __name__ == '__main__':
+    assign05_main()
+
